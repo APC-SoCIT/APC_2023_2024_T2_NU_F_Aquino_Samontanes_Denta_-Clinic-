d628443f8867cc4c62a56e79d728f0a5e7f62855
KaceyDalag <dalagkj@student.apc.edu.ph>
2024-01-07 22:50:30 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dental Homepage</title>
+    <title>Aquino Samontanes Dental Clinic</title>
     <link rel="stylesheet" href="css's/index.css">
     
 </head>
